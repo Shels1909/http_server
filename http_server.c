8827eca964481c24535cff03c7ec5b1e34c5336d
Shels1909 <knightmm@bob.knight-test.ch-geni-net.instageni.clemson.edu>
2017-09-03 22:33:18 -0400

    accept connection and print out buffer

--- http_server.c
+++ http_server.c
@@ -1,20 +1,40 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <strings.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <unistd.h>
 #define BUFFSIZE 32
 
 int main(int argc, char** argv){
 
-    int mysocket; 
+    int mysocket, conn, clilen, n; 
     struct sockaddr_in serv_addr, client_addr;
+    char buffer[BUFFSIZE];
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(3000);
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     mysocket = socket(AF_INET, SOCK_STREAM, 0);
     bind(mysocket, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
-    while(1)
-        listen(mysocket, 5);
+
+    listen(mysocket, 5);
+
+    clilen = sizeof(client_addr);
+
+    conn = accept(mysocket, (struct sockaddr*) &client_addr, &clilen);
+
+    if(conn < 0){
+        printf("ERROR ACCEPTING CONNECTION");
+    }
+    else{
+        printf("%d\n", conn);
+        printf("CONNTECTION ACCEPTED\n");
+    }
+
+    bzero(buffer, BUFFSIZE);
+    n = read(conn, buffer, BUFFSIZE);
+    printf("buffer:%s\n", buffer);
         
     return 0;
 
